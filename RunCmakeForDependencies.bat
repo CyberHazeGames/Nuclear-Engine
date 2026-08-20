5c56558e59b634f0a8daa22f4b7251594b8c682b
v7medz <v7medz@gmail.com>
2026-08-20 12:56:54 +0300

    Update CMake generator to Visual Studio 2026

--- RunCmakeForDependencies.bat
+++ RunCmakeForDependencies.bat
@@ -1,4 +1,4 @@
-cmake External\Assimp\ -B External\CmakeProjects\Assimp\ -G "Visual Studio 17 2022" -A x64
-cmake External\Diligent\ -B External\CmakeProjects\Diligent\ -G "Visual Studio 17 2022" -A x64
-cmake External\OpenAL\ -B External\CmakeProjects\OpenAL\ -G "Visual Studio 17 2022" -A x64
+cmake External\Assimp\ -B External\CmakeProjects\Assimp\ -G "Visual Studio 18 2026" -A x64
+cmake External\Diligent\ -B External\CmakeProjects\Diligent\ -G "Visual Studio 18 2026" -A x64 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake External\OpenAL\ -B External\CmakeProjects\OpenAL\ -G "Visual Studio 18 2026" -A x64 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 pause
